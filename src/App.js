f2b43ebaa5c55ee9ed57a122b5744e4c9492aaff
AakankshaSarode <aakankshasarode1@gmail.com>
2024-03-09 22:09:04 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 
 import './App.css';
+import AddProduct from './components/AddProduct';
+import Categories from './components/Categories';
 import Header from './components/Header';
 import Home from './components/Home';
+import LikedProducts from './components/LikedProducts';
+import Login from './components/Login';
+import ProductDetail from './components/ProductDetail';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div className="App">
            <Header />
            <Home />
+           <Signup />
+           <Login />
+           <AddProduct/>
+           <Categories />
+     <LikedProducts />
+     <ProductDetail/>
 
     </div>
   );

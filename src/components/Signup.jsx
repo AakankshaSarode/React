bda74a21d91c1412ed3d3e316a52f82d4b02627f
AakankshaSarode <aakankshasarode1@gmail.com>
2024-03-17 00:11:52 +0530

    design

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -6,10 +6,12 @@ import axios from "axios";
 const Signup = () => {
   const [username, setusername] = useState(" ");
   const [password, setpassword] = useState(" ");
+  const [mobile, setmobile] = useState(" ");
+  const [email, setemail] = useState(" ");
   const handleApi = () => {
     console.log({ username, password });
     const url = " http://localhost:4000/signup";
-    const data = { username, password };
+    const data = { username, password ,mobile,email};
     axios
       .post(url, data)
       .then((res) => {
@@ -26,28 +28,54 @@ const Signup = () => {
   return (
     <div>
       <Header />
-      welcome to signup page...
-      <br />
-      USERNAME
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => {
-          setusername(e.target.value);
-        }}
-      />
-      <br />
-      PASSWORD
-      <input
-        type="text"
-        value={password}
-        onChange={(e) => {
-          setpassword(e.target.value);
-        }}
-      />
-      <br />
-      <button onClick={handleApi}>SignUp</button>
-      <Link to="/login">LOGIN</Link>
+      <div className="p-3 m-3">
+       <h3> welcome to signup page...</h3>
+        <br />
+        USERNAME
+        <input
+          className="form-control"
+          type="text"
+          value={username}
+          onChange={(e) => {
+            setusername(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        MOBILE
+        <input
+          className="form-control"
+          type="text"
+          value={mobile}
+          onChange={(e) => {
+            setmobile(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        EMAIL
+        <input
+          className="form-control"
+          type="text"
+          value={email}
+          onChange={(e) => {
+            setemail(e.target.value);
+          }}
+        />
+        <br />
+        PASSWORD
+        <input
+          className="form-control"
+          type="text"
+          value={password}
+          onChange={(e) => {
+            setpassword(e.target.value);
+          }}
+        />
+        <br />
+        <button className="btn btn-primary m-3" onClick={handleApi}>SignUp</button>
+        <Link className="m-3"   to="/login">LOGIN</Link>
+      </div>
     </div>
   );
 };

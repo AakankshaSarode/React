17675d74fa103fae89498b3c24f6a4b9c273ea92
AakankshaSarode <aakankshasarode1@gmail.com>
2024-03-31 14:45:03 +0530

    done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,146 +1,162 @@
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
-import AddProduct from "./AddProduct";
 import Categories from "./Categories";
-import { FaHeart, FaShoppingCart } from "react-icons/fa"; // Import FaShoppingCart icon
-import "./Home.css";
+import { FaHeart } from "react-icons/fa";
+import './Home.css';
+import API_URL from "../constants";
 
-const Home = () => {
-  const navigate = useNavigate();
-  const [products, setProducts] = useState([]);
-  const [cProducts, setCProducts] = useState([]);
-  const [search, setSearch] = useState("");
-  const [isSearch, setIsSearch] = useState(false);
 
-  useEffect(() => {
-    const url = "http://localhost:4000/get-products";
-    axios
-      .get(url)
-      .then((res) => {
-        if (res.data.products) {
-          setProducts(res.data.products);
+function Home() {
+
+    const navigate = useNavigate()
+
+    const [products, setproducts] = useState([]);
+    const [cproducts, setcproducts] = useState([]);
+    const [search, setsearch] = useState('');
+    const [issearch, setissearch] = useState(false);
+
+    // useEffect(() => {
+    //     if (!localStorage.getItem('token')) {
+    //         navigate('/login')
+    //     }
+    // }, [])
+
+    useEffect(() => {
+        const url = API_URL + '/get-products';
+        axios.get(url)
+            .then((res) => {
+                if (res.data.products) {
+                    setproducts(res.data.products);
+                }
+            })
+            .catch((err) => {
+                alert('Server Err.')
+            })
+    }, [])
+
+    const handlesearch = (value) => {
+        setsearch(value);
+    }
+
+    const handleClick = () => {
+
+        const url = API_URL + '/search?search=' + search + '&loc=' + localStorage.getItem('userLoc');
+        axios.get(url)
+            .then((res) => {
+                setcproducts(res.data.products);
+                setissearch(true);
+            })
+            .catch((err) => {
+                alert('Server Err.')
+            })
+
+        // let filteredProducts = products.filter((item) => {
+        //     if (item.pname.toLowerCase().includes(search.toLowerCase()) ||
+        //         item.pdesc.toLowerCase().includes(search.toLowerCase()) ||
+        //         item.category.toLowerCase().includes(search.toLowerCase())) {
+        //         return item;
+        //     }
+        // })
+        // setcproducts(filteredProducts)
+
+    }
+
+    const handleCategory = (value) => {
+        let filteredProducts = products.filter((item, index) => {
+            if (item.category == value) {
+                return item;
+            }
+        })
+        setcproducts(filteredProducts)
+    }
+
+    const handleLike = (productId, e) => {
+        e.stopPropagation();
+        let userId = localStorage.getItem('userId');
+
+        if (!userId) {
+            alert('Please Login first.')
+            return;
         }
-      })
-      .catch((err) => {
-        alert("server err");
-      });
-  }, []);
 
-  const handleSearch = (value) => {
-    setSearch(value);
-  };
+        const url = API_URL + '/like-product';
+        const data = { userId, productId }
+        axios.post(url, data)
+            .then((res) => {
+                if (res.data.message) {
+                    alert('Liked.')
+                }
+            })
+            .catch((err) => {
+                alert('Server Err.')
+            })
 
-  const handleClick = () => {
-    const url = "http://localhost:4000/search?search=" + search;
+    }
 
-    axios
-      .get(url)
-      .then((res) => {
-        if (res.data.products) {
-          setCProducts(res.data.products);
-          setIsSearch(true); // Set isSearch to true when search is performed
-        } else {
-          setIsSearch(false); // Set isSearch to false if no products are found
-        }
-      })
-      .catch((err) => {
-        alert("server err.");
-      });
-  };
 
-  const handleCategory = (value) => {
-    let filteredProducts = products.filter((item, index) => {
-      return item.category === value;
-    });
-    setCProducts(filteredProducts);
-    setIsSearch(true); // Set isSearch to true when category is selected
-  };
+    const handleProduct = (id) => {
+        navigate('/product/' + id)
+    }
 
-  const handleLike = (productId) => {
-    let userId = localStorage.getItem("userId");
 
-    const url = "http://localhost:4000/like-product";
-    const data = { userId, productId };
-    axios
-      .post(url, data)
-      .then((res) => {
-        if (res.data.message) {
-          alert("Liked.");
-        }
-      })
-      .catch((err) => {
-        alert("server err.");
-      });
-  };
+    return (
+        <div>
+            <Header search={search} handlesearch={handlesearch} handleClick={handleClick} />
+            <Categories handleCategory={handleCategory} />
+            {issearch && cproducts &&
+                <h5> SEARCH RESULTS
+                    <button className="clear-btn" onClick={() => setissearch(false)}> CLEAR </button>
+                </h5>}
 
-  const handleProduct = (id) => {
-    navigate("/product/" + id);
-  };
+            {issearch && cproducts && cproducts.length == 0 && <h5> No Results Found </h5>}
+            {issearch && <div className="d-flex justify-content-center flex-wrap">
+                {cproducts && products.length > 0 &&
+                    cproducts.map((item, index) => {
 
-  const handleAddToCart = () => {
-    navigate("/liked-products"); // Redirect to Liked Products section
-  };
+                        return (
+                            <div key={item._id} className="pcard m-3 ">
+                                <div onClick={() => handleLike(item._id)} className="icon-con">
+                                    <FaHeart className="icons" />
+                                </div>
+                                <img width="300px" height="200px" src={API_URL + '/' + item.pimage} />
 
-  return (
-    <div>
-      <Header
-        search={search}
-        handleSearch={handleSearch}
-        handleClick={handleClick}
-      />
-      <Categories handleCategory={handleCategory} />
+                                <p className="m-2 pname"> {item.pname}  </p>
+                                
+                                <p className="m-2 pcategory"> {item.category} </p>
+                                <h3 className="m-2 pprice"> {item.price} </h3>
+                                <p className="m-2 pdesc"> {item.pdesc} </p>
+                            </div>
+                        )
 
-      {isSearch && cProducts.length > 0 && (
-        <div className="d-flex justify-content-center flex-wrap">
-          {cProducts.map((item) => (
-            <div key={item._id} className="pcard m-3">
-              <div onClick={() => handleLike(item._id)} className="icons-heart">
-                <FaHeart className="icons" />
-              </div>
-              <img
-                width="300px"
-                height="200px"
-                src={"http://localhost:4000/" + item.pimage}
-              />
-              <p className="pl-2">
-                {item.pname} | {item.category}
-              </p>
-              <p className="pl-2 text-success">{item.pdesc}</p>
-              <h3 className="pl-2 text-success">{item.price}</h3>
-            </div>
-          ))}
+                    })}
+            </div>}
+
+            {!issearch && <div className="d-flex justify-content-center flex-wrap">
+                {products && products.length > 0 &&
+                    products.map((item, index) => {
+
+                        return (
+                            <div onClick={() => handleProduct(item._id)} key={item._id} className="pcard m-3">
+                                   <div onClick={(e) => handleLike(item._id, e)} className="icon-con">
+                                    <FaHeart className="icons" />
+                                </div>
+                                <img width="250px" height="150px" src={API_URL + '/' + item.pimage} />
+      
+                                <p className="m-2 pname"> {item.pname}   </p>
+                                <p className="m-2 pcategory">   {item.category} </p>
+                                <h3 className="m-2 pprice"> Rs. {item.price} /- </h3>
+                                <p className="m-2 pdesc"> {item.pdesc} </p>
+                            </div>
+                        )
+
+                    })}
+            </div>}
+
         </div>
-      )}
-
-      {!isSearch && (
-        <div className="d-flex justify-content-center flex-wrap">
-          {products.map((item) => (
-            <div
-              key={item._id}
-              className="pcard m-3"
-              onClick={() => handleProduct(item._id)}
-            >
-               <div onClick={() => handleLike(item._id)} className="icon-con">
-                  <FaHeart className="icons" />
-               </div>
-               <img
-                 width="300px"
-                 height="200px"
-                 src={"http://localhost:4000/" + item.pimage}
-               />
-               <p className="pl-2 pname">{item.pname}</p>
-               <p className="pl-2 pcategory">{item.category}</p>
-               <h3 className="pl-2 pprice">Rs.{item.price}</h3>
-               <p className="pl-2  pdesc">{item.pdesc}</p>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-};
+    )
+}
 
 export default Home;
